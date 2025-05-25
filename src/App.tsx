6019e71f96f1c8a431c68d154994c7a9c3c389bb
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-05-25 23:37:18 +1000

    added home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Signin from "./signin"
+import Home from "./home"
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
