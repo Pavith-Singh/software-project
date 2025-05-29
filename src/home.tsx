d91eb584a20bf994159d07b34c32372d50879225
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-05-29 18:49:33 +1000

    trying to implement three.js integration

--- src/home.tsx
+++ src/home.tsx
@@ -1,16 +1,13 @@
 import Nav from './components/Nav/Nav'
+import ThreeScene from './components/Nav/ThreeScene'
 
 const Home: React.FC = () => {
   return (
     <div>
       <Nav />
       <div style={{ height: '60px' }} />
-      <h1>Homepage</h1>
-      <div style={{ height: '2000px', background: 'linear-gradient(white, #eee 80%, #ccc)' }}>
-        <p style={{ padding: '20px', fontSize: '1.2rem' }}>
-          Scroll down to test the sticky navbar. This content is just here to make the page scrollable.
-        </p>
-      </div>
+      <ThreeScene />
+      <h1 className='relative z-10'>Homepage</h1>
     </div>
   )
 }
