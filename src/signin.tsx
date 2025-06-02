a2ca1fc50824a5ff74e0a003b95bbeee17047023
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-06-02 23:17:38 +1000

    login page ui complete

--- src/signin.tsx
+++ src/signin.tsx
@@ -1,11 +1,14 @@
-import Nav from './components/Nav/Nav'
 
-const Signin = () => {
+import Nav from './components/Nav/Nav'
+import SigninComponent from './components/Nav/SigninComponent'
+
+
+function Signin() {
   return (
-    <div>
+    <div className='bg-gradient-to-b from-red-600 via-red-900 to-black w-full h-screen flex items-center justify-center relative'>
       <Nav />
-      <div style={{ height: '60px' }} />
-      <h1>Sign In</h1>
+      <div style={{height: '60px' }} />
+      <SigninComponent />
     </div>
   )
 }
