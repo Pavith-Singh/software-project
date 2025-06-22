a70426871875c6d6d65eab84356671c6a03de420
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-06-22 23:05:54 +1000

    moved database out of dist to prevent it from being rewritten

--- server/database.ts
+++ server/database.ts
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 
-const db = new Database(path.join(__dirname, 'student_world_social_network.db'), { verbose: console.log });
+const db = new Database(path.join(__dirname, '../student_world_social_network.db'), { verbose: console.log });
 
 db.prepare(`CREATE TABLE IF NOT EXISTS friend_requests (id INTEGER PRIMARY KEY AUTOINCREMENT, requester_uid TEXT NOT NULL, requested_uid TEXT NOT NULL, status TEXT CHECK(status IN ('pending','accepted','rejected')) NOT NULL DEFAULT 'pending', created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`).run();
 
