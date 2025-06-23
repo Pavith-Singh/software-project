ea9c73e900dfb79da2e7068309b10a060dea7334
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-06-23 12:07:38 +1000

    attempt 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signin from "./signin";
 import Home from "./home";
 import Support from "./contact";
